dbde1b225336a02929d8829a3636cbedb90ed944
renzaspiras <aspirasrenz@gmail.com>
2023-01-27 21:50:02 +0800

    Noir Release!

--- builds/BSEcdule/js/script.js
+++ builds/BSEcdule/js/script.js
@@ -26,9 +26,9 @@ function displaySchedule(){
 
 
 function constructor(){
-    dataFetch2()
+    dataFetch()
     controlWeek(true)
-    footer()
+    toggler()
     functionX()
 }
 
@@ -45,7 +45,37 @@ class uni_vars{
     
 }
 
-function dataFetch2(){
+function toggler (){
+    let footer = document.getElementById("toggler")    
+    acc = new uni_vars()        
+    let week = document.createElement("ul")
+    week.id = "linggo"
+    let today = acc.today()
+
+    let titlex = document.createElement("div")
+    titlex.id = "titlex"
+    titlex.innerHTML = today
+    footer.appendChild(titlex)
+
+    for(i = 0; i < acc.days.length; i++){
+        let day = document.createElement("button")
+        day.innerHTML = acc.days[i]
+        day.className = "araw"
+        day.id = "Sub" + acc.days[i]
+        if ("Sub" + today == "Sub" + acc.days[i]){
+            day.classList.add("today")
+        }
+        else{
+            day.classList.remove("today")
+        }
+        week.appendChild(day)
+
+    }
+    
+    footer.appendChild(week)    
+}
+
+function dataFetch(){
     let acc = new uni_vars()
     let main_section = document.getElementById("main_section")
     let scheduleData = data[document.title]; console.log(document.title)
@@ -57,7 +87,6 @@ function dataFetch2(){
         header.id = "header"
         containers.appendChild(header)
         main_section.appendChild(containers)
-
         
         for(let sub in scheduleData){
             if(scheduleData[sub]['Day'].includes(acc.days[i])){
@@ -128,82 +157,6 @@ function dataFetch2(){
     
 }
 
-function dataFetch(){
-    let main_section = document.getElementById("main_section")
-    let days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let scheduleData = data["BSIT_1B"];
-    for(i = 0; i < days.length; i++){        
-        let weekject = document.createElement("ol");
-        let weeklet = document.createElement("div");
-        weeklet.innerHTML = days[i]
-        weeklet.id = "rott" //--> ????
-        weekject.id = days[i]
-        main_section.appendChild(weekject)
-        weekject.appendChild(weeklet)
-
-        for (let sub in scheduleData) {
-            if (scheduleData[sub]['Day'].includes(days[i])) {
-                let cone = document.createElement("div")
-                cone.id = "cone"
-                let layerA000 = document.createElement("div")
-                cone.appendChild(layerA000)
-                layerA000.id = "layerA000"
-                
-                let subject = document.createElement("h4"); //--> Subject
-                subject.innerHTML = scheduleData[sub]['subject']  
-                subject.id = "subjects"
-                weekject.appendChild(cone)
-                layerA000.appendChild(subject)
-
-                //teacher
-                let teacher = document.createElement("ul")
-                teacher.innerHTML = scheduleData[sub]["Teacher"]
-                teacher.id = "teacher"
-                layerA000.appendChild(teacher)
-                
-                let ww = document.createElement("div")//--> where and when?
-                ww.id = "ww"
-                cone.appendChild(ww)
-                //room                
-                let room = document.createElement("ul")
-                room.innerHTML = "Room: " +  scheduleData[sub]["Room"]
-                room.id = "room"
-                ww.appendChild(room)
-                //time
-                let time_div = document.createElement("div")
-                time_div.id = "time"
-                ww.appendChild(time_div)
-                let timeA = document.createElement("ol")
-                timeA.innerHTML = scheduleData[sub]["Time"][0]
-                timeA.id = "time_start"
-                let timeB = document.createElement("ol")
-                timeB.innerHTML = scheduleData[sub]["Time"][1]
-                timeB.id = "time_end"
-                time_div.appendChild(timeA)
-                time_div.appendChild(timeB)
-
-                //image deck
-                let imgx = document.createElement("img")
-                imgx.id = "imgx"
-                imgx.src = "img/subs/" + scheduleData[sub]['subject'] + ".png"
-                cone.appendChild(imgx)
-                
-                let down_deck = document.createElement("div")
-                down_deck.id = "down_deck"
-                cone.appendChild(down_deck)
-                //class
-                let classC = document.createElement("ol")
-                classC.innerHTML = "Class: " + scheduleData[sub]["Class"]
-                down_deck.appendChild(classC)
-                //code
-                let codeC = document.createElement("ol")
-                codeC.innerHTML = "Code: " + scheduleData[sub]["Code"]
-                down_deck.appendChild(codeC)
-            }
-        }
-    }
-}
-
 
 function controlWeek(ba){
     if (ba == true) {
@@ -234,31 +187,6 @@ function weekSort(){
     
 }
 
-function footer (){
-    let footer = document.getElementById("footer")
-    acc = new uni_vars()
-    let week = document.createElement("ul")
-    week.id = "linggo"
-    let today = acc.today()
-    for(i = 0; i < acc.days.length; i++){
-        let day = document.createElement("button")
-        day.innerHTML = acc.days[i]
-        day.className = "araw"
-        day.id = "Sub" + acc.days[i]
-        if ("Sub" + today == "Sub" + acc.days[i]){
-            day.classList.add("today")
-        }
-        else{
-            day.classList.remove("today")
-        }
-        week.appendChild(day)
-
-    }
-    
-    footer.appendChild(week)
-    
-}
-
 function functionX(){
     acc = new uni_vars()
     for(i = 0; i < acc.Subday.length; i++){
@@ -276,13 +204,16 @@ function functionX(){
                     //console.log(which + " " + currentDay)
                     if (which == currentDay) {
                         //console.log("You clicked " + currentDay)
+                        
                         document.documentElement.scrollTop = 0;
+                        document.getElementById("titlex").textContent = which
                         document.getElementById("Sub" + currentDay).classList.add("today")
                         document.getElementById(currentDay).style.display = ""
                     }
                     else if(which != currentDay){
                         try {
                             document.documentElement.scrollTop = 0;
+                            document.getElementById("titlex").textContent = which
                             document.getElementById("Sub" + currentDay).classList.remove("today")
                             document.getElementById(currentDay).style.display = "none"
                         } catch (Uncaught) {
