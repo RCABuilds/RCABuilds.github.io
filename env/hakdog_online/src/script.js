d6db56ccd972e84c08519a91b0e7a559f05aa2ab
renzaspiras <aspirasrenz@gmail.com>
2023-08-16 01:24:06 +0800

    intro to the firebase

--- env/hakdog_online/src/script.js
+++ env/hakdog_online/src/script.js
@@ -1,4 +1,6 @@
 import { fav_icon, img, make, print, id, readJSONFile, openLink} from 'https://rcabuilds.github.io/src/js/rca.mjs';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
 
 
 
@@ -16,6 +18,13 @@ function header(){
 }
 
 function gate_keep(){
+    // Google Firebase
+    make("section").from("google_login").to("body").build()
+    make("h1").from("gate_keep_mess").to("#google_login").content("Welcome to Hakdog").build()
+    
+    make("div").from("container_A000").to("#google_login").build()
+        make()
+
     
 }
 
