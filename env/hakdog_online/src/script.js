b7e2b0dc98471e52b3b6bb5101968c78f343bfec
renzaspiras <aspirasrenz@gmail.com>
2023-08-15 22:54:10 +0800

    classess update

--- env/hakdog_online/src/script.js
+++ env/hakdog_online/src/script.js
@@ -1,12 +1,45 @@
-import { fav_icon, img, make, print, id, readJSONFile, openLink} from './rca.mjs';
+import { fav_icon, img, make, print, id, readJSONFile, openLink} from 'https://rcabuilds.github.io/src/js/rca.mjs';
 
 
 
 
 function constructor(){
+    header()
+    body()
+}
+constructor()
 
+
+function header(){
+    fav_icon("./../env/hakdog_online/assets/hotdog.webp");
 }
 
 function body(){
+    // All
 
+    // Sunday
+
+    // Monday
+
+    // Tuesday
+
+    // Wednesday
+
+    // Thursday
+
+    // Friday
+
+    // Saturday
+
+    // Holiday Plugin
+
+    // Time Plugin
+
+    // Current Schedule
+
+    // Scrollable
+
+    // Current Button
+
+    // Bible 
 }
